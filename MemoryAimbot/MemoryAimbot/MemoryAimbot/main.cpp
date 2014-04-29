35ee4c61669950f5747d0846d448ad2bfa8c832d
Rahim Mitha <rkmitha@gmail.com>
2014-04-29 12:13:46 -0400

    Init

---
+++ MemoryAimbot/MemoryAimbot/MemoryAimbot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MemoryAimbot
+//
+//  Created by Rahim Mitha on 2014-04-29.
+//  Copyright (c) 2014 Rahim Mitha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
